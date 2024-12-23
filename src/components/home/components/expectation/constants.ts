9f527ccf8983b8d6f2c4d3bceb9b86acc98d8298
Rahul Verma <rahul.v@sprinklr.com>
2024-12-23 20:23:18 +0530

    expectations section

---
+++ src/components/home/components/expectation/constants.ts
@@ -0,0 +1,47 @@
+import exp1 from "../../../../../public/exp1.png";
+import exp2 from "../../../../../public/exp2.png";
+import exp3 from "../../../../../public/exp3.png";
+import exp4 from "../../../../../public/exp4.png";
+
+export const EXPECTATION = [
+  {
+    id: "1",
+    title: "Zap mode",
+    description:
+      "Enjoy a game that matches your taste or keep swiping until you discover one that excites you.",
+    image: exp1,
+    borderColor: "border-exp-1",
+    textColor: "text-exp-1",
+    imageBgColor: "bg-exp-1",
+  },
+  {
+    id: "2",
+    title: "Zip mode",
+    description:
+      "All achievements in Zap Mode are automatically saved, allowing you to resume later or explore any game from our ever-expanding library of top-tier titles.",
+    image: exp2,
+    borderColor: "border-exp-2",
+    textColor: "text-exp-2",
+    imageBgColor: "bg-exp-2",
+  },
+  {
+    id: "3",
+    title: "Zoom mode",
+    description:
+      "Unleash your creativity with our AI-assisted game creation tool and launch new games faster than ever.",
+    image: exp3,
+    borderColor: "border-exp-3",
+    textColor: "text-exp-3",
+    imageBgColor: "bg-exp-3",
+  },
+  {
+    id: "4",
+    title: "Custom creations",
+    description:
+      "Transform popular games into customized versions with our intuitive prompt-based tool. Share your creations with friends and enjoy playing together!",
+    image: exp4,
+    borderColor: "border-exp-4",
+    textColor: "text-exp-4",
+    imageBgColor: "bg-exp-4",
+  },
+];
